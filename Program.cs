03c46b52dd528f07535db05821c33e31187dc991
Aleksander Zverev <aleksander.zverev.lt@gmail.com>
2020-07-07 14:46:09 +0500

    DI-Container. Only Ninject.Modules

--- Program.cs
+++ Program.cs
@@ -25,81 +25,15 @@ namespace CommandLineTool
 
     public class Program
     {
-        private static ICommandsExecutor CreateExecutorUsingModules()
+        private static ICommandsExecutor CreateExecutor()
         {
             var container = new StandardKernel(new CommandsModule());
             return container.Get<ICommandsExecutor>();
         }
 
-        private static ICommandsExecutor CreateExecutor()
-        {
-            var container = new StandardKernel();
-
-            #region Варианты биндинга CommandExecutor-а
-            /*
-            container.Bind<ICommandsExecutor>().To<CommandsExecutor>().InSingletonScope();
-            */
-
-            /*
-            container.Bind<ICommandsExecutor>().To<CommandsExecutor>()
-                .InSingletonScope()
-                .WithConstructorArgument((TextWriter)new RedTextConsoleWriter());
-            */
-
-            /*
-            container.Bind<ICommandsExecutor>().To<CommandsExecutor>()
-                .InSingletonScope()
-                //"При обращении к аргументу конструктора TextWriter присвой пожалуйста следующий тип"
-                .WithConstructorArgument(typeof(TextWriter), c => c.Kernel.Get<RedTextConsoleWriter>());
-            */
-
-            // /*
-            container.Bind(c => c.FromThisAssembly().SelectAllClasses().BindAllInterfaces());
-            // */
-            #endregion
-
-            #region Варианты биндинга комманд
-            /*
-            container.Bind<ConsoleCommand>().To<PrintTimeCommand>();
-            container.Bind<ConsoleCommand>().To<TimerCommand>();
-            container.Bind<ConsoleCommand>().To<HelpCommand>();
-            container.Bind<ConsoleCommand>().To<DetailedHelpCommand>();
-            */
-
-            // /*
-            container.Bind(c => c.FromThisAssembly().SelectAllClasses().BindAllBaseClasses());
-            // */
-            #endregion
-
-            #region Варианты биндинга TextWriter-ов
-            /*
-            //Единый TextWriter
-            container.Bind<TextWriter>().ToConstant(Console.Out);
-            */
-
-            //TextWriter для консольных команд
-            container.Bind<TextWriter>().To<PromptConsoleWriter>()
-                .WhenInjectedInto<ConsoleCommand>();
-
-            // /*
-            //TextWriter для Executor-а
-            container.Bind<TextWriter>().To<RedTextConsoleWriter>()
-                .WhenInjectedInto<ICommandsExecutor>();
-            // */
-
-            /*
-            //Биндинг с использование атрибута Named.
-            container.Bind<TextWriter>().To<RedTextConsoleWriter>().Named("error");
-            */
-            #endregion
-
-            return container.Get<ICommandsExecutor>();
-        }
-
         static void Main(string[] args)
         {
-            //ICommandsExecutor executor = CreateExecutor(); 
-            ICommandsExecutor executor = CreateExecutorUsingModules();
+            ICommandsExecutor executor = CreateExecutor();
             if (args.Length > 0)
                 executor.Execute(args);
             else
